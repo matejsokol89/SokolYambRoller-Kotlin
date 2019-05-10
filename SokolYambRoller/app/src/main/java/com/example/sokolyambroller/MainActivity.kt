5dabf2874e88bc2a257c78546d4d78dad4ca2a51
Matej Sokol <sokolvm@gmail.com>
2019-05-10 12:54:07 +0200

    Sredio svu logiku, setiram clickove, nedam vise od 3 i nakon clicka setiram na nulu countera

--- SokolYambRoller/app/src/main/java/com/example/sokolyambroller/MainActivity.kt
+++ SokolYambRoller/app/src/main/java/com/example/sokolyambroller/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.sokolyambroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var counter = 1
+    var counter = 0
 
     lateinit var firstCube: ImageView
     lateinit var secondCube: ImageView
@@ -15,6 +16,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var fourthCube: ImageView
     lateinit var fiftCube: ImageView
     lateinit var sixtCube: ImageView
+
+    var firstCubeSelected = false
+    var secondCubeSelected = false
+    var thirdCubeSelected = false
+    var fourthCubeSelected = false
+    var fifthCubeSelected = false
+    var sixthCubeSelected = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,98 +36,190 @@ class MainActivity : AppCompatActivity() {
         fiftCube = findViewById(R.id.cube5)
         sixtCube = findViewById(R.id.cube6)
 
+        firstCube.setOnClickListener {
+            firstCubeSelected = when (firstCubeSelected) {
+                true -> false
+                false -> true
+
+            }
+            counter++
+            print("ovo je ")
+            println(counter)
+        }
+
+
+        secondCube.setOnClickListener {
+            secondCubeSelected = when (secondCubeSelected) {
+                true -> false
+                false -> true
+            }
+            counter++
+            print("ovo je ")
+            println(counter)
+        }
+
+        thirdCube.setOnClickListener {
+            thirdCubeSelected = when (thirdCubeSelected) {
+                true -> false
+                false -> true
+            }
+            counter++
+            print("ovo je ")
+            println(counter)
+        }
+
+        fourthCube.setOnClickListener {
+            fourthCubeSelected = when (fourthCubeSelected) {
+                true -> false
+                false -> true
+            }
+            counter++
+            print("ovo je ")
+            println(counter)
+        }
+
+        fiftCube.setOnClickListener {
+            fifthCubeSelected = when (fifthCubeSelected) {
+                true -> false
+                false -> true
+            }
+            counter++
+            print("ovo je ")
+            println(counter)
+        }
+
+        sixtCube.setOnClickListener {
+            sixthCubeSelected = when (sixthCubeSelected) {
+                true -> false
+                false -> true
+            }
+            counter++
+            print("ovo je ")
+            println(counter)
+        }
 
 
         fun rollFirstDice() {
-            val drawableResource = when (Random().nextInt(6) + 1) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            if (!firstCubeSelected) {
+                val drawableResource = when (Random().nextInt(6) + 1) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                firstCube.setImageResource(drawableResource)
             }
-            firstCube.setImageResource(drawableResource)
-
 
         }
+
 
         fun rollSecondDice() {
-            val drawableResource = when (Random().nextInt(6) + 1) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            if (!secondCubeSelected) {
+                val drawableResource = when (Random().nextInt(6) + 1) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                secondCube.setImageResource(drawableResource)
             }
-            secondCube.setImageResource(drawableResource)
 
         }
+
 
         fun rollThirdDice() {
-            val drawableResource = when (Random().nextInt(6) + 1) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            if (!thirdCubeSelected) {
+                val drawableResource = when (Random().nextInt(6) + 1) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                thirdCube.setImageResource(drawableResource)
             }
-            thirdCube.setImageResource(drawableResource)
 
         }
 
+
         fun rollFourthdDice() {
-            val drawableResource = when (Random().nextInt(6) + 1) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            if (!fourthCubeSelected) {
+                val drawableResource = when (Random().nextInt(6) + 1) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                fourthCube.setImageResource(drawableResource)
             }
-            fourthCube.setImageResource(drawableResource)
 
         }
 
         fun rollFiftdDice() {
-            val drawableResource = when (Random().nextInt(6) + 1) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            if (!fifthCubeSelected) {
+                val drawableResource = when (Random().nextInt(6) + 1) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                fiftCube.setImageResource(drawableResource)
             }
-            fiftCube.setImageResource(drawableResource)
 
         }
 
+
         fun rollSixthdDice() {
-            val drawableResource = when (Random().nextInt(6) + 1) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            if (!sixthCubeSelected) {
+                val drawableResource = when (Random().nextInt(6) + 1) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                sixtCube.setImageResource(drawableResource)
             }
-            sixtCube.setImageResource(drawableResource)
 
         }
 
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            counter=2
-            rollFirstDice()
-            rollSecondDice()
-            rollThirdDice()
-            rollFourthdDice()
-            rollFiftdDice()
-            rollSixthdDice()
+            if (counter > 2) {
+                Toast.makeText(this, "Only two dices can be selected", Toast.LENGTH_SHORT).show();
+
+            } else {
+                rollFirstDice()
+                rollSecondDice()
+                rollThirdDice()
+                rollFourthdDice()
+                rollFiftdDice()
+                rollSixthdDice()
+            }
+            counter = 0
+            firstCubeSelected = false
+            secondCubeSelected = false
+            thirdCubeSelected = false
+            fourthCubeSelected = false
+            fifthCubeSelected = false
+            sixthCubeSelected = false
+
+
         }
 
+
     }
+
 }
+
